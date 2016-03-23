2ecea9476e1b2c74f434bf45078200002f48878b
LIUYONG <liuyong@liuyongs-mbp.lan>
2016-03-23 13:15:41 +0800

    Initial Commit

---
+++ BanTang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BanTang
+//
+//  Created by LIUYONG on 3/23/16.
+//  Copyright © 2016 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BanTang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BanTang
+//
+//  Created by LIUYONG on 3/23/16.
+//  Copyright © 2016 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
